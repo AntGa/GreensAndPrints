fb4302e609d75d1dac7160d5978dcf8276cbbefb
Anton Garay <antonvince.garay@gmail.com>
2024-07-24 22:39:02 +1200

    Initial Commit ( tulza bullies me )

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Header } from './components/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
