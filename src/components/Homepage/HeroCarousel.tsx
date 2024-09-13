f62bb07984439c3ad56eb418824acb52cd963f94
Raymond Garay <raymond.garay@one.nz>
2024-09-14 10:03:04 +1200

    Feat: Redesign Homepage

--- src/components/Homepage/HeroCarousel.tsx
+++ src/components/Homepage/HeroCarousel.tsx
@@ -1,52 +1,22 @@
-'use client'
-import React, { useCallback } from 'react'
-import useEmblaCarousel from 'embla-carousel-react'
+'use client';
+import React, { useCallback } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
 
 const HeroCarousel = () => {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
-
-  const scrollPrev = useCallback(() => {
-    if (emblaApi) emblaApi.scrollPrev()
-  }, [emblaApi])
-
-  const scrollNext = useCallback(() => {
-    if (emblaApi) emblaApi.scrollNext()
-  }, [emblaApi])
-
   return (
-    <div className="relative">
-      <div className="embla h-[100vh] w-full overflow-hidden" ref={emblaRef}>
-        <div className="embla__container flex">
-          <div className="embla__slide h-[100vh] flex-[0_0_100%] bg-deep-forest-green">
-            Slide 1
-          </div>
-          <div className="embla__slide bg-soft-ivory h-[100vh] flex-[0_0_100%]">
-            Slide 2
-          </div>
-          <div className="embla__slide bg-soft-ivory h-[100vh] flex-[0_0_100%]">
-            Slide 3
-          </div>
-          <div className="embla__slide bg-soft-ivory h-[100vh] flex-[0_0_100%]">
-            Slide 4
-          </div>
+    <div className='h-dvh w-full overflow-hidden'>
+      <div className='big-image'>
+        <div className='flex h-full w-full flex-col items-center justify-center p-4 text-center text-white'>
+          <h1 className='text-3xl font-semibold md:text-5xl'>
+            Green Up Your Life with Style.
+          </h1>
+          <p className='mt-2 text-2xl md:text-4xl'>
+            Discover our Greenery and Prints Collections
+          </p>
         </div>
       </div>
-
-      <button
-        onClick={scrollPrev}
-        className="absolute left-4 top-1/2 -translate-y-1/2 transform rounded-full bg-white p-2 text-black"
-      >
-        Prev
-      </button>
-
-      <button
-        onClick={scrollNext}
-        className="absolute right-4 top-1/2 -translate-y-1/2 transform rounded-full bg-white p-2 text-black"
-      >
-        Next
-      </button>
     </div>
-  )
-}
+  );
+};
 
-export default HeroCarousel
+export default HeroCarousel;
