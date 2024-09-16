697a4e6fde7800f3952bc1db6a6d1ecff85786bc
Anton Garay <antonvince.garay@gmail.com>
2024-09-16 17:19:44 +1200

    Chore: Swapped to pnpm

--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,7 +1,7 @@
 import Collections from '@/components/layout/search/collections'
 import FilterList from '@/components/layout/search/filter'
 import { sorting } from '@/lib/constants'
-
+import ChildrenWrapper from './children-wrapper'
 export default function SearchLayout({
   children,
 }: {
@@ -13,7 +13,7 @@ export default function SearchLayout({
         <Collections />
       </div>
       <div className="order-last min-h-screen w-full md:order-none">
-        {children}
+        <ChildrenWrapper>{children}</ChildrenWrapper>
       </div>
       <div className="order-none flex-none md:order-last md:w-[125px]">
         <FilterList list={sorting} title="Sort by" />
--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -3,6 +3,7 @@ import Grid from '@/components/grid'
 export default function Loading() {
   return (
     <>
+      <div className="mb-4 h-6" />
       <Grid className="grid-cols-2 lg:grid-cols-3">
         {Array(12)
           .fill(0)
