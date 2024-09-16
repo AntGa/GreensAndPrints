5ee3c4daa5418815c00f183b0897ef7feacbcc17
Anton Garay <antonvince.garay@gmail.com>
2024-09-16 18:24:13 +1200

    Feat: Added search and cart functionality and checkout functionality (need to just style now)

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import React, { useState, useEffect } from 'react'
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion'
 import { usePathname } from 'next/navigation' // Import usePathname
+import Search from './search'
+import CartModal from '@/components/cart/modal'
 
 export const Navbar = () => {
   const { scrollY } = useScroll()
@@ -115,20 +117,10 @@ export const Navbar = () => {
           Greens & Prints
         </Link>
         <div className="flex gap-9 text-lg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="m-7 size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
-            />
-          </svg>
+          <Search />
+          <div className="flex justify-end md:w-1/3">
+            <CartModal />
+          </div>
         </div>
       </div>
       <motion.div
--- src/components/layout/Navbar/search.tsx
+++ src/components/layout/Navbar/search.tsx
@@ -8,23 +8,25 @@ export default function Search() {
   const searchParams = useSearchParams()
 
   return (
-    <Form
-      action="/search"
-      className="w-max-[550px] relative w-full lg:w-80 xl:w-full"
-    >
-      <input
-        key={searchParams?.get('q')}
-        type="text"
-        name="q"
-        placeholder="Search for products..."
-        autoComplete="off"
-        defaultValue={searchParams?.get('q') || ''}
-        className="text-md w-full rounded-lg border bg-white px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
-      />
-      <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
-        <MagnifyingGlassIcon className="h-4" />
-      </div>
-    </Form>
+    <div className="hidden items-center justify-center xl:flex">
+      <Form
+        action="/search"
+        className="w-max-[550px] relative w-full lg:w-80 xl:w-full"
+      >
+        <input
+          key={searchParams?.get('q')}
+          type="text"
+          name="q"
+          placeholder="Search for products..."
+          autoComplete="off"
+          defaultValue={searchParams?.get('q') || ''}
+          className="text-md w-full rounded-lg border px-4 py-2 text-black placeholder:text-neutral-500 md:text-sm dark:border-neutral-800 dark:text-white dark:placeholder:text-neutral-400"
+        />
+        <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
+          <MagnifyingGlassIcon className="h-4" />
+        </div>
+      </Form>
+    </div>
   )
 }
 
@@ -33,7 +35,7 @@ export function SearchSkeleton() {
     <form className="w-max-[550px] relative w-full lg:w-80 xl:w-full">
       <input
         placeholder="Search for products..."
-        className="w-full rounded-lg border bg-white px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:bg-transparent dark:text-white dark:placeholder:text-neutral-400"
+        className="w-full rounded-lg border px-4 py-2 text-sm text-black placeholder:text-neutral-500 dark:border-neutral-800 dark:text-white dark:placeholder:text-neutral-400"
       />
       <div className="absolute right-0 top-0 mr-3 flex h-full items-center">
         <MagnifyingGlassIcon className="h-4" />
