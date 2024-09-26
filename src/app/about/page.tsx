36dc72a8694cdc5191ab8ef149dfea88684af6c9
Raymond Garay <raymond.garay@one.nz>
2024-09-27 10:11:16 +1200

    Feat: Initialized About Page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div className='mt-[80px]'>page</div>;
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import HeroBanner from '@/components/Homepage/HeroBanner'
-import OurFavourites from '@/components/Homepage/OurFavourites/OurFavourites'
-import { ValuesBanner } from '@/components/Homepage/ValuesBanner/ValuesBanner'
+import HeroBanner from '@/components/Homepage/HeroBanner';
+import OurFavourites from '@/components/Homepage/OurFavourites/OurFavourites';
+import { ValuesBanner } from '@/components/Homepage/ValuesBanner/ValuesBanner';
 
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-soft-ivory">
-        <div className="flex flex-col">
+      <div className='min-h-screen bg-soft-ivory'>
+        <div className='flex flex-col'>
           <HeroBanner />
           <ValuesBanner />
           <OurFavourites />
         </div>
       </div>
     </>
-  )
+  );
 }
