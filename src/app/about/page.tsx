a6d5fdfa9996a6ce93b0b494f3d69e60b543f10a
Raymond Garay <raymond.garay@one.nz>
2024-09-27 12:52:49 +1200

    About Us Teaser

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import { AboutIntro } from '@/components/about/intro';
 import React from 'react';
 
 const page = () => {
-  return <div className='mt-[80px]'>page</div>;
+  return (
+    <div className='mt-[80px]'>
+      <AboutIntro />
+    </div>
+  );
 };
 
 export default page;
